5def00c4f752202ba4104c2e0f918a948d2c32a6
mimo <archmime@gmail.com>
2020-11-06 14:41:00 -0300

    agregado carruosel de imagenes, fijado el mapa, cambiado el logo, cambiado los iconos de la lista por botones

--- index.js
+++ index.js
@@ -23,27 +23,12 @@ function setMapa(zona){
 }
 
 
-function esVisible(elem){
-    var posTopView = window.scrollY;
-    var posButView = posTopView + window.innerHeight;
-    var elemTop = elem.offsetTop;
-    var elemBottom = elemTop + elem.offsetHeight;
 
-    if (posTopView > 200) {
-        elem.classList.add("mapaFixed")
-        elem.style.left = elem.getBoundingClientRect().x
-    }
-
-
-    console.log(elem.getBoundingClientRect())
-    return {postop: posTopView, posbut: posButView, elemtop:elemTop, elemb: elemBottom}
-    //((elemBottom < posButView && elemBottom > posTopView) || (elemTop >posTopView && elemTop< posButView));
-}
-
-var ele =  document.getElementById("desc")
-
-var x = 0;
-function myFunction() {
-  document.getElementById("demo").innerHTML = x += 1;
-  console.log(esVisible(ele))
-}
+document.getElementById('lista').onclick = function changeContent() {
+    
+    var contenedor =  document.getElementById("desc")
+    
+    console.log(contenedor.getBoundingClientRect().height)
+    
+    contenedor.style.height = contenedor.getBoundingClientRect().height
+ }
