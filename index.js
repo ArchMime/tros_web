1070535c87c31181bdeada7bb7abd54db8f002f1
mimo <archmime@gmail.com>
2020-11-11 08:55:35 -0300

    rediseñado las imagenes del carrusel, logo, y limpiado codigo

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function setMapa(zona){
+function setMapa(zona) {
 
     var image
 
     switch (zona) {
-        case "norte":
-            image ="img/Mapa_norte_Gran_Buenos_Aires.png"
+        case "n":
+            image = "img/Mapa_norte_Gran_Buenos_Aires.png"
             break;
 
-        case "oeste":
-            image="img/Mapa_oeste_Gran_Buenos_Aires.png"
+        case "o":
+            image = "img/Mapa_oeste_Gran_Buenos_Aires.png"
             break;
-    
-        case"sur":
-            image="img/Mapa_sur_Gran_Buenos_Aires.png"
+
+        case "s":
+            image = "img/Mapa_sur_Gran_Buenos_Aires.png"
             break;
 
         default:
@@ -21,14 +21,3 @@ function setMapa(zona){
 
     document.getElementById("mapa").src = image
 }
-
-
-
-document.getElementById('lista').onclick = function changeContent() {
-    
-    var contenedor =  document.getElementById("desc")
-    
-    console.log(contenedor.getBoundingClientRect().height)
-    
-    contenedor.style.height = contenedor.getBoundingClientRect().height
- }
